352a3ca72bebfeb93dc1d8e6a03fb09d37dbdb9d
AmNotAGoose <alexyihengwu@gmail.com>
2024-08-17 13:57:18 -0400

    added basic login page and navigation to /draw. added provider for user/auth.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,19 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App.jsx'
 import FrontPage from "./FrontPage.jsx"
+import LoginPage from "./LoginPage.jsx"
+import { AuthProvider } from './AuthProvider.jsx';
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>  
-      <Route path="/" element={<FrontPage />}/>
-      <Route path="/draw" element={<App />}/>
-    </Routes>
-  </BrowserRouter>,
+createRoot(document.getElementById('root')).render
+(
+  <AuthProvider>
+    <BrowserRouter>
+      <Routes>  
+        <Route path="/" element={<FrontPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/draw" element={<App />}/>
+      </Routes>
+    </BrowserRouter>
+  </AuthProvider>,
 )
